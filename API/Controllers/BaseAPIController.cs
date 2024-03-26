f0c6f857da1437e85585c642e49e652f5f6df4a6
Pedro <Pedro@DESKTOP-LS19V4N>
2024-03-26 12:16:32 -0300

    Add project files.

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseAPIController : ControllerBase
+    {
+    }
+}
