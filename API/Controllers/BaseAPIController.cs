4fcdfede91e64fe6ea208c78dddd139f89c4a902
pedromedici-uvv <146857181+pedromedici-uvv@users.noreply.github.com>
2024-05-03 09:59:16 -0300

    CRUD CQRS Mediator

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,5 +9,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseAPIController : ControllerBase
     {
+
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= 
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
